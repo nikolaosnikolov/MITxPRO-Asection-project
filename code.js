71884f7535f14138bfec32abbac3c07521268632
nikos.nikolov <nikos.nikolov@mtel.gr>
2022-06-13 12:04:25 +0300

    updates

--- code.js
+++ code.js
@@ -3,20 +3,36 @@ const home = document.getElementById('home-script')
 const info = document.getElementById('info-script')
 const projects = document.getElementById('projects-script')
 const contact = document.getElementById('contact-script')
-const contactForm = document.getElementById('contact-data')
-const contactWelcome = document.getElementById('welcome-contact')
+const visitContact = document.getElementById('visit-contact')
+const infoPage = document.getElementById('bio-start')
+const contactPage = document.getElementById('contact-start')
+const homePage = document.getElementById('home-start')
+const contactButtonAtHome = document.getElementById('contact-button')
 
-contactForm.style.setProperty('display', 'none')
-contactWelcome.style.setProperty('display', 'none')
+infoPage.style.setProperty('display', 'none')
+contactPage.style.setProperty('display', 'none')
+homePage.style.setProperty('display', 'none')
 
 home.onclick = () => {
-    // const youtubeVideo = document.createElement('iframe')
-    // youtubeVideo.src = 'https://www.youtube.com/embed/qRPMAyQxrFY'
-    // youtubeVideo.setAttribute('id', 'yt-video')
-    // mainContainer.appendChild(youtubeVideo)
+    homePage.style.removeProperty('display')
+    infoPage.style.setProperty('display', 'none')
+    contactPage.style.setProperty('display', 'none')
+}
+
+info.onclick = () => {
+    infoPage.style.removeProperty('display')
+    contactPage.style.setProperty('display', 'none')
+    homePage.style.setProperty('display', 'none')
 }
 
 contact.onclick = () => {
-    contactForm.style.removeProperty('display')
-    contactWelcome.style.removeProperty('display')
+    contactPage.style.removeProperty('display')
+    infoPage.style.setProperty('display', 'none')
+    homePage.style.setProperty('display', 'none')
+}
+
+contactButtonAtHome.onclick = () => {
+    contactPage.style.removeProperty('display')
+    infoPage.style.setProperty('display', 'none')
+    homePage.style.setProperty('display', 'none')
 }
