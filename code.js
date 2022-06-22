7d16abc45c11197872ea68865a1cd51e3ee08775
nikolaosnikolov <nikos.nikolov@mtel.gr>
2022-06-22 19:49:35 +0300

    updates

--- code.js
+++ code.js
@@ -37,21 +37,21 @@ modalTitle.style.fontSize = '1.3rem'
 
 
 
-image7.onclick = () => {
-    modal.style.display = "block";
-    modalTitle.textContent = 'PacMen Factory'
-    modalBody.textContent = 'In this project you can see how PacMen are moving inside the screen corners. They are created by clicking the button that adds PacMen. The movement starts when clicking the button that starts the procedure. When PacMen hit corners they change direction, so no PacMan gets out of page borders.'
-}
-image8.onclick = () => {
-    modal.style.display = "block";
-    modalTitle.textContent = 'Eye Movement'
-    modalBody.textContent = 'In this project the two eyes that are presented on your webpage are moving in relationship with the movement of your mouse. Wherever you locate your mouse, the eyes are looking to it.'
-}
-image9.onclick = () => {
-    modal.style.display = "block";
-    modalTitle.textContent = 'Real Time Bus Tracker'
-    modalBody.textContent = 'In this project, by clicking the button that starts the route you are going to see a marker located at every single stop. Also, this stop is written on the header so you understand where the bus is. Moreover, all the data used are taken live by Washington Metropolitan Area Transit.'
-}
+// image7.onclick = () => {
+//     modal.style.display = "block";
+//     modalTitle.textContent = 'PacMen Factory'
+//     modalBody.textContent = 'In this project you can see how PacMen are moving inside the screen corners. They are created by clicking the button that adds PacMen. The movement starts when clicking the button that starts the procedure. When PacMen hit corners they change direction, so no PacMan gets out of page borders.'
+// }
+// image8.onclick = () => {
+//     modal.style.display = "block";
+//     modalTitle.textContent = 'Eye Movement'
+//     modalBody.textContent = 'In this project the two eyes that are presented on your webpage are moving in relationship with the movement of your mouse. Wherever you locate your mouse, the eyes are looking to it.'
+// }
+// image9.onclick = () => {
+//     modal.style.display = "block";
+//     modalTitle.textContent = 'Real Time Bus Tracker'
+//     modalBody.textContent = 'In this project, by clicking the button that starts the route you are going to see a marker located at every single stop. Also, this stop is written on the header so you understand where the bus is. Moreover, all the data used are taken live by Washington Metropolitan Area Transit.'
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
