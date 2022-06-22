41d82aea2cffc09d77c033c1a0b5ddb88d756d61
nikolaosnikolov <nikos.nikolov@mtel.gr>
2022-06-22 17:27:24 +0300

    updates

--- code.js
+++ code.js
@@ -9,40 +9,62 @@ const contactPage = document.getElementById('contact-start')
 const homePage = document.getElementById('home-start')
 const projectsPage = document.getElementById('projects-start')
 const contactButtonAtHome = document.getElementById('contact-button')
-const projectsModal = document.getElementById('staticBackdrop')
-const modalTitle = document.getElementById('staticBackdropLabel')
-const modalBody = document.getElementById('modal-body')
-const realProject7 = document.getElementById('real-project7')
-const realProject8 = document.getElementById('real-project8')
-const realProject9 = document.getElementById('real-project9')
 
 
-realProject7.onclick = () => {
-    modalTitle.textContent = 'PacMen Factory'
-    modalBody.textContent = 'In this project you can see how PacMen are moving inside the screen corners. They are created by clicking the button that adds PacMen. The movement starts when clicking the button that starts the procedure. When PacMen hit corners they change direction, so no PacMan gets out of page borders.'
-}
-realProject8.onclick = () => {
-    modalTitle.textContent = 'Eye Movement'
-    modalBody.textContent = 'In this project the two eyes that are presented on your webpage are moving in relationship with the movement of your mouse. Wherever you locate your mouse, the eyes are looking to it.'
-}
-realProject9.onclick = () => {
-    modalTitle.textContent = 'Real Time Bus Tracker'
-    modalBody.textContent = 'In this project, by clicking the button that starts the route you are going to see a marker located at every single stop. Also, this stop is written on the header so you understand where the bus is. Moreover, all the data used are taken live by Washington Metropolitan Area Transit.'
-}
 
-// realProject7.onclick = () => {
-
-//     modalTitle.textContent = modalContentArray[0][1]
-//     modalBody.textContent = modalContentArray[0][3]
-// }
+// Get the modal
+const modal = document.getElementById("myModal");
 
 
-// projectsModal.style.setProperty('display', 'none')
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+
+const modalTitle = document.getElementById('own-modal-title')
+const modalBody = document.getElementById('own-modal-body')
+const image7 = document.getElementById('project-image-7')
+const image8 = document.getElementById('project-image-8')
+const image9 = document.getElementById('project-image-9')
 
 infoPage.style.setProperty('display', 'none')
 contactPage.style.setProperty('display', 'none')
 homePage.style.setProperty('display', 'none')
 projectsPage.style.setProperty('display', 'none')
+
+
+modalTitle.style.fontSize = '1.3rem'
+
+
+
+
+image7.onclick = () => {
+    modal.style.display = "block";
+    modalTitle.textContent = 'PacMen Factory'
+    modalBody.textContent = 'In this project you can see how PacMen are moving inside the screen corners. They are created by clicking the button that adds PacMen. The movement starts when clicking the button that starts the procedure. When PacMen hit corners they change direction, so no PacMan gets out of page borders.'
+}
+image8.onclick = () => {
+    modal.style.display = "block";
+    modalTitle.textContent = 'Eye Movement'
+    modalBody.textContent = 'In this project the two eyes that are presented on your webpage are moving in relationship with the movement of your mouse. Wherever you locate your mouse, the eyes are looking to it.'
+}
+image9.onclick = () => {
+    modal.style.display = "block";
+    modalTitle.textContent = 'Real Time Bus Tracker'
+    modalBody.textContent = 'In this project, by clicking the button that starts the route you are going to see a marker located at every single stop. Also, this stop is written on the header so you understand where the bus is. Moreover, all the data used are taken live by Washington Metropolitan Area Transit.'
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 home.onclick = () => {
     homePage.style.removeProperty('display')
